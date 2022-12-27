beb56d4a37755f07188183276dc24f88f70e8a66
Mitch Henline <rmhenline@gmail.com>
2022-12-26 17:45:04 -0700

    working on log in

--- crud.py
+++ crud.py
@@ -27,6 +27,9 @@ def create_future_concert(band_name, genre, band_pic_path, user):
 def get_user_by_id(user_id):
     return User.query.get(user_id)
 
+def get_user_by_email(email):
+    return User.query.filter(User.email == email).first()
+
 if __name__ == '__main__':
     from server import app
     connect_to_db(app)
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, flash, session, redirect
 from model import connect_to_db, db
 import crud
 from jinja2 import StrictUndefined
+from forms import LoginForm
 
 app = Flask(__name__)
 app.secret_key= "kilby"
@@ -12,9 +13,34 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def homepage():
     """view homepage."""
+    if 'email' not in session:
+        return redirect('/login')
 
 
     return render_template('homepage.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """log user into site"""
+    form = LoginForm(request.form)
+
+    # Form is submitted with valid data
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+    #Check to see if a user with this email exists
+        user = crud.get_user_by_email(email)
+
+        if not user or user['password'] != password:
+            flash("Invalid email or password")
+            return redirect('/login')
+
+        session['username'] = user['username']
+        flash('Logged in.')
+        return redirect('/')
+
+    return render_template("login.html", form=form)
 
 @app.route('/concerts')
 def see_concerts(user_id):
