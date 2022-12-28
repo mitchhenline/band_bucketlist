15b181a9110275957f6840f26b3342590adb3f48
Mitch Henline <rmhenline@gmail.com>
2022-12-28 11:00:31 -0700

    login working

--- crud.py
+++ crud.py
@@ -27,7 +27,7 @@ def create_future_concert(band_name, genre, band_pic_path, user):
 def get_user_by_id(user_id):
     return User.query.get(user_id)
 
-def get_user_by_email(email):
+def get_user_by_email(email) -> User:
     return User.query.filter(User.email == email).first()
 
 
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Server for concert app."""
 
 from flask import Flask, render_template, request, flash, session, redirect
-from model import connect_to_db, db
+from model import connect_to_db, db, User
 import crud
 from jinja2 import StrictUndefined
 from forms import LoginForm
@@ -31,14 +31,11 @@ def login():
 
     #Check to see if a user with this email exists
         user = crud.get_user_by_email(email)
-        print("next line")
-        print(password)
-        print(user)
-        if not user or user['password'] != password:
-            # flash("Invalid email or password")
+        if not user or user.password != password:
+            flash("Invalid email or password")
             return redirect('/login')
 
-        session['username'] = user['username']
+        session['email'] = user.email
         flash('Logged in.')
         return redirect('/')
 
@@ -47,16 +44,20 @@ def login():
 @app.route('/concerts')
 def see_concerts(user_id):
     """view seen concert list."""
+    if 'email' not in session:
+        return redirect('/login')
 
-    user = crud.get_user_by_id(user_id)
+    user = crud.get_user_by_email(email)
 
     return render_template('concerts.html', user = user)
 
 @app.route('/bucketlist')
 def see_future_concerts(user_id):
     """view bucket list."""
+    if 'email' not in session:
+        return redirect('/login')
 
-    user = crud.get_user_by_id(user_id)
+    user = crud.get_user_by_email(email)
 
     return render_template('bucketlist.html', user = user)
 
