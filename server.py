9f8122855d87525c8a08ee4a1830ed66a69602f1
Mitch Henline <rmhenline@gmail.com>
2022-12-30 13:54:17 -0700

    add bands working

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 """Server for concert app."""
 
-from flask import Flask, render_template, request, flash, session, redirect
+from flask import Flask, render_template, request, flash, session, redirect, abort
 from model import connect_to_db, db, User, Concert
 import crud
 from jinja2 import StrictUndefined
 from forms import LoginForm, AddConcert
-from crud import add_concert
+from crud import add_concert, get_user_by_email
 
 app = Flask(__name__)
 app.secret_key= "kilby"
@@ -49,25 +49,34 @@ def logout():
     flash("Logged out.")
     return redirect("/login")
 
-@app.route('/concerts')
+@app.route('/concerts', methods=["GET"])
 def see_concerts():
     """view seen concert list."""
-
+    form = AddConcert(request.form)
     if 'email' not in session:
         return redirect('/login')
-
     concerts = crud.get_concerts(session["email"])
-    form = AddConcert(request.form)
-
-    if form.validate_on_submit():
-        band_name = form.band_name.data
-        date = form.date.data
-        venue = form.venue.data
-        location = form.location.data
-        band_pic_path = form.band_pic_path.data
-
     return render_template('concerts.html', concerts = concerts, form=form)
 
+@app.route('/concerts', methods=["POST"])
+def post_concert():
+    form = AddConcert(request.form)
+    if 'email' not in session:
+        return redirect('/login')
+    if form.validate_on_submit():
+        concert = Concert(
+            band_name = form.band_name.data,
+            date = form.date.data,
+            venue = form.venue.data,
+            location = form.location.data,
+            band_pic_path = form.band_pic_path.data,
+            user_id = get_user_by_email(session["email"]).user_id
+        )
+        db.session.add(concert)
+        db.session.commit()
+        return redirect("/concerts")
+    else:
+        abort(404)
 # @app.route("/add_concert", methods=["POST"])
 # def add_concert():
 
